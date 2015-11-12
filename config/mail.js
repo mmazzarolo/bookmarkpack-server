3cb356de68f4bddefca84a81fd21e81e08807ca6
Matteo Mazzarolo <mazzarolomatteo@gmail.com>
2015-11-12 17:11:55 +0100

    Update mail.js

--- config/mail.js
+++ config/mail.js
@@ -3,12 +3,12 @@
 exports.resetMail = function(email, token) {
   return {
     to: email,
-    from: 'bookmark@pack.com',
+    from: 'https://bookmarkpack.herokuapp.com',
     subject: 'Reset your password on BookmarkPack',
     text:
       'You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n' +
       'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-      'http://localhost:8000/#/reset/' + token + '\n\n' +
+      'https://bookmarkpack.herokuapp.com/#/reset/' + token + '\n\n' +
       'If you did not request this, please ignore this email and your password will remain unchanged.\n'
   }
 }
@@ -16,7 +16,7 @@ exports.resetMail = function(email, token) {
 exports.resetConfirmMail = function(email) {
   return {
     to: email,
-    from: 'bookmark@pack.com',
+    from: 'https://bookmarkpack.herokuapp.com',
     subject: 'Your BookmarkPack password has been changed',
     text:
       'Hello,\n\n' +
@@ -27,12 +27,12 @@ exports.resetConfirmMail = function(email) {
 exports.verifyMail = function(email, token) {
   return {
     to: email,
-    from: 'bookmark@pack.com',
+    from: 'https://bookmarkpack.herokuapp.com',
     subject: 'Verify your account',
     text:
       'You are receiving this email because you (or someone else) have registered a new account on BookmarkPack.\n\n' +
       'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-      'http://localhost:8000/#/verify/' + token + '\n\n' +
+      'https://bookmarkpack.herokuapp.com/#/verify/' + token + '\n\n' +
       'If you did not request this, please ignore this email and the account will be deleted.\n'
   }
 }
